b40ad69c1899a9aa45ae55693a005d68c50c5816
kun qian <81524741+sedimentary-cloud@users.noreply.github.com>
2021-08-06 11:44:15 +0800

    Generate GUI.desktop automatically

--- GUIMonitor.sh
+++ GUIMonitor.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+# generate GUI.desktop
+echo "[Desktop Entry]
+Encoding=UTF-8 
+Name=Anesthesia Monitor
+Exec=$(pwd)/GUIMonitor.sh
+Icon=$(pwd)/monitor.ico
+Terminal=true
+StartupNotify=true
+Type=Application">GUI.desktop
+
+# run roscore
+source /opt/ros/noetic/setup.bash
 gnome-terminal -t "roscore" -x bash -c "roscore"
 
+sleep 1s
+
+# run other nodes
 cd DataReceiver/
-bash runDataReceiver.sh
+bash dataReceiver.sh
 
 cd ..
 cd Qt-interface
-bash runQT.sh
+bash GUI.sh
 
 cd build-SmartAn-Desktop_Qt_5_14_2_GCC_64bit-Debug
 ./SmartAn
