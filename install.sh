68acf028205bf328fe23326e806e24f8a5c23597
sd-cloud <mnbvcxz12l@163.com>
2021-08-07 12:45:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+sudo apt install pip
+pip3 install hl7parser
+pip3 install sockets
+pip3 install rospy
+
+
+chmod 777 /DataReceiver/dataReceiver.sh
+chmod 777 /DataReceiver/src/monitor/scripts/main.py
+chmod 777 /Qt-interface/GUI.sh
+chmod 777 /Qt-interface/SmartMessage/src/smart_topic/src/anesthesia_subscriber.sh
+
 # generate GUI.desktop
 echo "[Desktop Entry]
 # Encoding=UTF-8 
@@ -10,12 +21,6 @@ Icon=$(pwd)/monitor.ico
 Terminal=true
 StartupNotify=true
 Type=Application">GUI.desktop
-
-chmod 777 GUIMonitor.sh
-chmod 777 /DataReceiver/dataReceiver.sh
-chmod 777 /DataReceiver/src/monitor/scripts/main.py
-chmod 777 /Qt-interface/GUI.sh
-chmod 777 /Qt-interface/SmartMessage/src/smart_topic/src/anesthesia_subscriber.sh
 
 sudo cp $(pwd)/GUI.desktop /usr/share/applications
 rm $(pwd)/GUI.desktop
