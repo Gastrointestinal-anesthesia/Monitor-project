e82a7a0f549b4cfb2e9aaa251163bf0ea98b4b35
sd-cloud <mnbvcxz12l@163.com>
2021-08-07 14:02:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # build workspace
-cd /Qt-interface/SmartMessage
+cd Qt-interface/SmartMessage
 catkin_make
 cd ..
 cd ..
-cd /DataReceiver
+cd DataReceiver/
 catkin_make
 cd ..
 
 # change mode
-chmod 777 /DataReceiver/dataReceiver.sh
-chmod 777 /DataReceiver/src/monitor/scripts/main.py
-chmod 777 /Qt-interface/GUI.sh
-chmod 777 /Qt-interface/SmartMessage/src/smart_topic/src/anesthesia_subscriber.sh
+chmod 777 DataReceiver/dataReceiver.sh
+chmod 777 DataReceiver/src/monitor/scripts/main.py
+chmod 777 Qt-interface/GUI.sh
+chmod 777 Qt-interface/SmartMessage/src/smart_topic/src/anesthesia_subscriber.sh
 
 # generate GUI.desktop
 echo "[Desktop Entry]
@@ -26,6 +26,6 @@ Terminal=true
 StartupNotify=true
 Type=Application">GUI.desktop
 
-sudo mv /GUI.desktop /usr/share/applications
+sudo mv GUI.desktop /usr/share/applications
 chmod 777 /usr/share/applications/GUI.desktop
 
